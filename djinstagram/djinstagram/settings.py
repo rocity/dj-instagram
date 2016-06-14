bfe9e95690502c45978a7f926a40c29d63b2fb30
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-06-14 16:12:09 +0800

    Added common files

--- djinstagram/djinstagram/settings.py
+++ djinstagram/djinstagram/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'instaapp.apps.BlogConfig',
+    'instaapp.apps.InstaappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
