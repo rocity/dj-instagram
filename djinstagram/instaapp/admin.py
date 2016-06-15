b95609a051dd0f11d77c78c5a6a81bd8f73ebfda
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-06-15 10:18:17 +0800

    Added models to admin view

--- djinstagram/instaapp/admin.py
+++ djinstagram/instaapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Photo, Like, Tag
+
+class PhotoAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Photo
+    '''
+    list_display = ('owner', 'caption', 'path', 'created')
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Like)
+admin.site.register(Tag)
--- djinstagram/instaapp/models.py
+++ djinstagram/instaapp/models.py
@@ -3,10 +3,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Photo(models.Model):
     owner = models.ForeignKey(User, null=True)
     caption = models.TextField(max_length=255)
     path = models.TextField(max_length=255)
+    tags = models.ManyToManyField(Tag)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -21,10 +29,3 @@ class Like(models.Model):
 
     def __str__(self):
         return self.photo
-
-class Tag(models.Model):
-    name = models.TextField(max_length=255)
-    photo = models.ManyToManyField(Photo)
-
-    def __str__(self):
-        return self.name
