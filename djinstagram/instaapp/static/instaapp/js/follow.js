8622b6a2feb3591e80093d35dd2dd7be37eb197d
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2016-10-05 09:42:34 +0800

    Add hover event handlers on Following button

--- djinstagram/instaapp/static/instaapp/js/follow.js
+++ djinstagram/instaapp/static/instaapp/js/follow.js
@@ -3,7 +3,7 @@
 * File where every script that is related to the model `Follow` will be here
 ******************/
 $(document).ready(function() {
-  $('.btn-follow').on('click', function(e) {
+  $('.btn-follow').on('click', function (e) {
     e.preventDefault();
 
     var t = $(this),
@@ -23,4 +23,38 @@ $(document).ready(function() {
       }
     });
   });
-});
+
+  /*
+  * .btn-following Events
+  * This button is found in the page where the logged user can view
+  * the list of users he/she is following.
+  * If the user hovers on this button, it should turn "red" and show a text
+  * that says "Unfollow"
+  */
+  function followingHoverIn(e) {
+    var t = $(this),
+        span = t.find('span.glyphicon'),
+        text = t.find('span.text');
+
+    if (t.hasClass('btn-primary') && span.hasClass('glyphicon-ok')) {
+      t.removeClass('btn-primary').addClass('btn-danger');
+      span.removeClass('glyphicon-ok').addClass('glyphicon-remove');
+      text.text('Unfollow');
+    }
+  }
+
+  function followingHoverOut(e) {
+    var t = $(this),
+        span = t.find('span.glyphicon'),
+        text = t.find('span.text');
+
+    if (t.hasClass('btn-danger') && span.hasClass('glyphicon-remove')) {
+      t.addClass('btn-primary').removeClass('btn-danger');
+      span.removeClass('glyphicon-remove').addClass('glyphicon-ok');
+      text.text('Following');
+    }
+  }
+
+  $('.btn-following').hover(followingHoverIn, followingHoverOut);
+
+}); // document ready
