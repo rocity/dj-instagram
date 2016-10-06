7515c095e16987457c0330f26e65dc5fc5aaa978
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2016-10-06 08:25:05 +0800

    Split views into two files for ajax and general use views

--- djinstagram/djinstagram/urls.py
+++ djinstagram/djinstagram/urls.py
@@ -19,10 +19,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from instaapp import views
+from instaapp.views import general
 
 urlpatterns = [
-    url(r'^$', views.index, name='instaindex'),
+    url(r'^$', general.index, name='instaindex'),
     url(r'^admin/', admin.site.urls),
     url(r'^insta/', include('instaapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djinstagram/instaapp/urls.py
+++ djinstagram/instaapp/urls.py
@@ -1,24 +1,25 @@
 from django.conf.urls import url
 
-from . import views
+from .views import ajax
+from .views import general
 
 app_name = 'instaapp'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.user_login, name='user_login'),
-    url(r'^logout/$', views.user_logout, name='user_logout'),
-    url(r'^upload/$', views.upload_photo, name='upload_photo'),
-    url(r'^users/$', views.users, name='users'),
-    url(r'^u/(?P<username>[\w-]+)$', views.user_profile, name='user_view'),
-    url(r'^profile/$', views.user_profile, name='user_profile'),
-    url(r'^following/$', views.user_following, name='user_following'),
-    url(r'^followers/$', views.user_followers, name='user_followers'),
-    url(r'^feed/$', views.feed, name='feed'),
+    url(r'^$', general.index, name='index'),
+    url(r'^login/$', general.user_login, name='user_login'),
+    url(r'^logout/$', general.user_logout, name='user_logout'),
+    url(r'^upload/$', general.upload_photo, name='upload_photo'),
+    url(r'^users/$', general.users, name='users'),
+    url(r'^u/(?P<username>[\w-]+)$', general.user_profile, name='user_view'),
+    url(r'^profile/$', general.user_profile, name='user_profile'),
+    url(r'^following/$', general.user_following, name='user_following'),
+    url(r'^followers/$', general.user_followers, name='user_followers'),
+    url(r'^feed/$', general.feed, name='feed'),
 
     # AJAX methods
-    url(r'^upload_dp/$', views.upload_user_profile_pic,
+    url(r'^upload_dp/$', ajax.upload_user_profile_pic,
         name='upload_user_profile_pic'),
-    url(r'^follow/$', views.follow_user, name='follow_user'),
-    url(r'^post_comment/$', views.post_photo_comment, name='post_photo_comment'),
-    url(r'^like_comment/$', views.like_photo, name='like_photo'),
+    url(r'^follow/$', ajax.follow_user, name='follow_user'),
+    url(r'^post_comment/$', ajax.post_photo_comment, name='post_photo_comment'),
+    url(r'^like_comment/$', ajax.like_photo, name='like_photo'),
 ]
